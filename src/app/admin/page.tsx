76f6dcb5962a80dc53b17bab8aecde66b598cb9e
ryszardwhee <ryszrzepa@gmail.com>
2024-08-15 15:09:16 +0200

    add password to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,30 @@
+'use client'
 import ScrapeDataForm from "@/app/admin/scrape-data-form";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-const Admin = async () => {
+const Admin = () => {
+  const password = "mao-assistant";
+  const [showPage, setShowPage] = useState(false);
+  const [passwordInput, setPasswordInput] = useState("");
+
   return (
     <div className="w-full mx-auto p-12">
-      <h3>Scrape data from a url</h3>
-      <ScrapeDataForm />
+      {showPage ? (<div>
+        <h3>Scrape data from a url</h3>
+        <ScrapeDataForm/>
+      </div>) : (<div className="flex gap-4 flex-col">
+        <h3>Type password</h3>
+        <Input onChange={(e) => {
+          setPasswordInput(e.target.value)
+        }}/>
+        <Button onClick={() => {
+          if (passwordInput === password) {
+            setShowPage(true);
+          }
+        }}>Submit</Button>
+      </div>)}
     </div>
   );
 };
